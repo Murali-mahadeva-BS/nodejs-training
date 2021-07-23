21f41ad427401907869a0b09b050b8dd33c58731
Murali  mahadeva B S <muralimahadev40@gmail.com>
2021-07-23 17:58:24 +0530

    fs and path modules

--- index.js
+++ index.js
@@ -1,9 +1,32 @@
-const express = require("express");
-const app = express();
+const path = require("path");
+const fs = require("fs");
 
-app.get("/", (req, res) => {
-  res.write("hello world ");
-  res.end();
-});
+// path module
+// let temp = path.join(__dirname, "some", "someting", "some.txt");
+// console.log("path: ", temp);
+// let temp = path.resolve("some", "something"); console.log("path:", temp);
+// console.log("abosule path:", __dirname);
 
-app.listen(8080, () => console.log("server listening on port:", 8080));
+// fs module
+// reading from a file
+// fs.readFile("test.txt", (err, data) => {
+//   if (err) return console.log("Error in reading file");
+//   console.log("data:", data.toString());
+// });
+
+// writing to a file
+// fs.writeFile("test.txt", "hello ", (err) => {
+//   if (err) return console.log("Error in writng file");
+// });
+
+// appending data to file
+// fs.appendFile("test.txt", "hello ", (err) => {
+//   if (err) return console.log("Error in appending file");
+//   console.log("file appended succesfully");
+// });
+
+// delete file
+// fs.unlink("test.txt", (err) => {
+//   if (err) return console.log("Error in deleting file");
+//   console.log("file deleted succesfully");
+// });
