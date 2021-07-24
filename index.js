a12cb60e36a4615bc2ea7c503a5e2aac9625110e
Murali  mahadeva B S <muralimahadev40@gmail.com>
2021-07-24 19:23:49 +0530

    getting data from client updated

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 let todos = [];
+
 app.get("/", (req, res) => {
-  res.render("todo");
-  res.end();
+  res.render(path.resolve("views", "todo.html"));
 });
+
 app.post("/", (req, res) => {
   let { body } = req;
   console.log("body from html:", body);
