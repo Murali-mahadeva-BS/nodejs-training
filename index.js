7e239bf93e9b72dcc84651144e50718526b79da0
Murali  mahadeva B S <muralimahadev40@gmail.com>
2021-07-24 17:41:08 +0530

    getting data from html

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 const express = require("express");
-const ejs = require("ejs");
 const app = express();
-
+app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
-// app.set("views", __dirname + "/pages");
 app.use(express.static("public"));
-
+let todos = [];
 app.get("/", (req, res) => {
-  res.render("index");
+  res.render("todo");
   res.end();
 });
-
-app.listen(8080, () => console.log("server is listenig on port:", 8080));
+app.post("/", (req, res) => {
+  let { body } = req;
+  console.log("body from html:", body);
+  todos.push(body.todo);
+  console.log("todo:", todos);
+  res.redirect("/");
+});
+app.listen(8080, () => console.log("listening on port 8080"));
