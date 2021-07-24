52f02351278c7d985e1776dd31e284aadb9305d3
Murali  mahadeva B S <muralimahadev40@gmail.com>
2021-07-24 17:42:34 +0530

    injecting data into html using ejs template engine

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 const express = require("express");
+const ejs = require("ejs");
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 let todos = [];
 app.get("/", (req, res) => {
-  res.render("todo");
+  res.render("todo", { todos });
   res.end();
 });
 app.post("/", (req, res) => {
   let { body } = req;
   console.log("body from html:", body);
   todos.push(body.todo);
-  console.log("todo:", todos);
+  console.log("todos:", todos);
   res.redirect("/");
 });
 app.listen(8080, () => console.log("listening on port 8080"));
